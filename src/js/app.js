2837d6f335ea1678ff90e06df0c7a87686130843
Jordan <jordancolehunt@gmail.com>
2016-01-24 11:45:24 +0200

    moved neighborhoods data into JSON file and used $.getJSON to fetch and parse file

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,47 @@
 	google maps API implementation
 	------------------------------------------------------------
 */
+// // jQuery function to get JSON via AJAX call using JSON-P
+// var neighborhoods = $.ajax("http://www.jordancolehunt.co.za/data_udacity_app/data.json", {
+// 	dataType: "jsonp",
+// 	jsonp: "callback",
+// 	jsonpCallback: "jsonCallback",
+// 	type: "GET"
+// 	});
+// function jsonCallback(data) {
+// 	console.log(data);
+// }
+
+var neighborhoods = [];
+// jQuery function to get JSON which parses it into JS Object literal
+// for loop required to push each object into array sequentially
+/*
+	1. TO DO: refactor for loop into forEach method
+	2. Change error handling to notify user about failure
+*/
+var neighborhoodsJSON = $.getJSON("js/data/data.json", function(data){
+	var length = data.neighborhoods.length;
+	for (var i = 0; i < length; i++){
+		neighborhoods.push(data.neighborhoods[i]);
+	}
+}).fail(function() {
+	console.log('failed to load neighborhood data');
+});
+
+
+
+
 var map;
 var markers = [];
 var infowindow = [];
+
 // create array of neighborhoods  areas
-var neighborhoods = [
-{lat: -29.748909, lng: 31.058702, title: "fuck", content: 'hi'},
-{lat: -29.726029, lng: 31.084880, title: "yeah", content: 'how'},
-{lat: -29.785487, lng: 31.020924, title: "bro", content: 'you'}
-];
+// var neighborhoods = [
+// {lat: -29.748909, lng: 31.058702, title: "fuck", content: 'hi'},
+// {lat: -29.726029, lng: 31.084880, title: "yeah", content: 'how'},
+// {lat: -29.785487, lng: 31.020924, title: "bro", content: 'you'}
+// ];
+
 // function to initialise google map
 	function initMap() {
 	// initialise map with coords and zoom level
